983f98b11bf2d427b073e18a78ed1350203806be
Christine Olukere <143913697+christineolukere@users.noreply.github.com>
2025-07-28 20:57:56 -0400

    Updated eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -26,3 +26,24 @@ export default tseslint.config(
     },
   }
 );
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDe7NBRuxfJ7h3OugtzFYXGOgxdNHvbifY",
+  authDomain: "shameless-1dfd9.firebaseapp.com",
+  projectId: "shameless-1dfd9",
+  storageBucket: "shameless-1dfd9.firebasestorage.app",
+  messagingSenderId: "246345924644",
+  appId: "1:246345924644:web:0ad17b320b6559fcec58b1",
+  measurementId: "G-NQJSJWJPKG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
